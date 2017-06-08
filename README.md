deecc7609be7bb74840d831f9bec8403e3acb888
aishu7 <aishjelly@gmail.com>
2017-06-08 17:56:18 +0530

    optimized code

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ Student Details
 This Project is for students to register their data. In this student can add their datas and they can see their datas.
 
 
-We have two buttons to handle the datas.
+We have three buttons to handle the data.
 1)ADD STUDENT
 2)DISPLAY STUDENT
+3)DISPLAY ALL STUDENT
 
 Description
 -----------
 
-When we click the ADD button it will redirect to next page, in that student have to enter valid and correct details. After entering details when we click save button it will save into our database. If student click cancel button then their datas would be considered to registered student. Student can able to see their details by giving their first name.
+When we click the ADD STUDENT button it will redirect to next page,student have to enter valid and correct details in that form. After entering details when we click save button it will save into our database. If student click cancel button then their data wont be considered as registered student. Student can able to see their details by using DISPLAY STUDENT button with their corresponding firstname then it will display the student details with the given firstname or DISPLAY ALL STUDENT button for displaying all the student details. Students can able to delete the details by clicking the DELETE button and edit the details by clicking the EDIT button. 
 
--- lib/StudentDetails/Controller/Manage.pm
+++ lib/StudentDetails/Controller/Manage.pm
@@ -22,7 +22,7 @@ Catalyst Controller.
 
 =item index
 
-Params : NONE
+Params : Save_or_cancel
 
 Returns: NONE
 
@@ -39,6 +39,7 @@ sub index : Local {
     if ( $save_or_cancel == 1 ) {
         $c->stash( button_click => 'save' );
     }
+
     elsif ( $save_or_cancel == 2 ) {
         $c->stash( button_click => 'cancel' );
     }
@@ -65,13 +66,11 @@ sub manage_data : Local {
     elsif ( $c->req->param('show') ) {
         $c->stash( template => 'display/display_form.tt' );
     }
-    elsif ( $c->req->param('display_all'))
-{
- $c->visit('display_data');}
+
+    elsif ( $c->req->param('display_all') ) {
+        $c->visit('display_data');
+    }
 }
-
-
-
 
 =item add_data
 
@@ -100,21 +99,33 @@ sub add_data : Local {
     my $student_result_set = $c->model('StudentDB::Student');
     if ( $c->req->method() eq "POST" && $c->req->param('save') ) {
 
-        #adding info into student database
-        my $result = $student_result_set->create(
-            {
-                first_name     => "$first",
-                last_name      => "$last",
-                gender         => "$gender",
-                date_of_birth  => "$dob",
-                contact_number => "$contact_num",
-                address        => "$address",
-                course         => "$course",
-            }
-        );
+        my $student_data = {
+            first_name     => "$first",
+            last_name      => "$last",
+            gender         => "$gender",
+            date_of_birth  => "$dob",
+            contact_number => "$contact_num",
+            address        => "$address",
+            course         => "$course",
+        };
 
-        $c->log->debug("Student Data Successfully saved");
+        if ( $c->req->param('id') ) {
+            my $student_result_set =
+              $c->model('StudentDB::Student')
+              ->find( { student_id => $c->req->param('id') } )
+              ->update($student_data);
+            $c->log->debug("Student Data Successfully Updated");
+
+        }
+
+        else {
+            #adding info into student database
+            my $result = $student_result_set->create($student_data);
+
+            $c->log->debug("Student Data Successfully Saved");
+        }
         $save_or_cancel = 1;
+
         $c->visit( 'index', [$save_or_cancel] );
     }
 
@@ -136,22 +147,21 @@ Desc : display_data is to get values from user and search into student database 
 =cut
 
 sub display_data : Local {
-    my ( $self, $c ) = @_;
-    my $first          = $c->req->param('first_name');
-    my $rs             = $c->model('StudentDB::Student');
-   my $student_result;
-    if($first)
-{
-     $student_result = $rs->search(
-        {
-            first_name => "$first"
-        }
-    );
-}
-else
-{
- $student_result =$rs->search();
-}
+    my ( $self, $c,$first_name ) = @_;
+    my $first = $first_name|$c->req->param('first_name');
+    my $rs    = $c->model('StudentDB::Student');
+    my $student_result;
+    if ($first) {
+        $student_result = $rs->search(
+            {
+                first_name => "$first"
+            }
+        );
+      $c->stash(first_name => "$first");
+    }
+    else {
+        $student_result = $rs->search();
+    }
     my @search_result;
     while ( my $i = $student_result->next ) {
         push @search_result,
@@ -173,23 +183,44 @@ else
     );
 }
 
+=item delete_row
+
+Params : NULL
+
+Returns: NULL   
+
+Desc : delete_row is to delete values from database corresponding to the row which the user selects to delete
+
+=cut
+
 sub delete_row : Local {
     my ( $self, $c ) = @_;
     my $delete_student = $c->model('StudentDB::Student')
       ->find( { student_id => $c->req->param('delete') } )->delete();
- $c->response->redirect('http://localhost:3000/manage/display_data');
+    my $first_name=$c->req->param('first_name');
+    $c->response->redirect($c->uri_for("/manage/display_data",$first_name));
 
 }
 
+=item select_row
+
+Params : NULL
+
+Returns: NULL   
+
+Desc : select_row is to select the row which the user selects the row for updating.
+
+=cut
+
 sub select_row : Local {
     my ( $self, $c ) = @_;
-   my $student_id = $c->req->param('update');
-    my $single_student_data = $c->model('StudentDB::Student')
-      ->search( { student_id => $student_id } );
-  my %student_result; 
-   while(my $i = $single_student_data->next) { 
-       %student_result =    (
-            id => $i->student_id,
+    my $student_id = $c->req->param('update');
+    my $single_student_data =
+      $c->model('StudentDB::Student')->search( { student_id => $student_id } );
+    my %student_result;
+    while ( my $i = $single_student_data->next ) {
+        %student_result = (
+            id          => $i->student_id,
             first_name  => $i->first_name,
             last_name   => $i->last_name,
             gender      => $i->gender,
@@ -197,34 +228,24 @@ sub select_row : Local {
             contact_num => $i->contact_number,
             address     => $i->address,
             course      => $i->course
-          );
-}
-$c->stash(template =>'details/add_details_update.tt',
-          result => \%student_result);
-  
-    }
-
-   
-
-
-
-sub update_data : Local {
-    my ( $self, $c ) = @_;
-    if ( $c->req->param('save') ) {
-     my $student_update = $c->model('StudentDB::Student')->find({ student_id => $c->req->param('id')})->update(
-            {
-                first_name  => $c->req->param('first_name'),
-                last_name   => $c->req->param('last_name'),
-                gender => $c->req->param('gender'),
-                date_of_birth         => $c->req->param('dob'),
-                contact_number => $c->req->param('num'),
-                address     => $c->req->param('address'),
-                course      => $c->req->param('course')
-            }
         );
     }
-$c->visit('index');
+    $c->stash(
+        template => 'details/add_details.tt',
+        result   => \%student_result
+    );
+
 }
+
+=item redirect_home_page
+
+Params : NULL
+
+Returns: NULL   
+
+Desc : redirect_home_page is to go back to home page when the user sets back button 
+
+=cut
 
 sub redirect_home_page : Local {
     my ( $self, $c ) = @_;
